1ee5379777e77a74f824216e716faf52c338afb9
Kirill Ivanov <iwwwanow.work@yandex.ru>
2025-11-06 19:24:12 +0300

    fix(bot): message response

--- apps/bot/application/src/use-cases/handle-message.use-case.ts
+++ apps/bot/application/src/use-cases/handle-message.use-case.ts
@@ -5,15 +5,7 @@ export class MessageResponse {
 export class HandleMessageUseCase {
   execute(): MessageResponse {
     // TODO locales
-    const commands = [
-      "/start - Начать работу с ботом",
-      "/help - Показать список команд",
-      "/download <url> - Скачать трек по ссылке",
-      "/status - Показать статус загрузок",
-    ];
-
-    const helpText = `🎵 **Music Downloader Bot**\n\nДоступные команды:\n${commands.join("\n")}`;
-
-    return new MessageResponse(helpText);
+    const messageResponse = `message-response`;
+    return new MessageResponse(messageResponse);
   }
 }
