833a62ef180ad171beb5a6b4cb2c6fbfcad9d9a9
Kirill Ivanov <iwwwanow.work@yandex.ru>
2025-11-17 20:25:02 +0300

    feat(telegram-bot): command props

--- apps/telegram-bot/application/src/dtos/commands/schedule-download.command.ts
+++ apps/telegram-bot/application/src/dtos/commands/schedule-download.command.ts
@@ -1,7 +1,14 @@
 import { Command } from "./base.command";
 
 export class ScheduleDownloadCommand extends Command {
-  constructor() {
+  constructor(
+    // TODO valueobject
+    public readonly resourceUrl: string,
+    // TODO valueobject
+    public readonly chatId: string,
+    // TODO valueobject
+    public readonly messageId: string
+  ) {
     super()
   }
 }
