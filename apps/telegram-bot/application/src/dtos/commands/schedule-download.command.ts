2f1b2e508c1b23741cdfe927c5b0f031059b5641
Kirill Ivanov <iwwwanow.work@yandex.ru>
2025-11-17 21:11:25 +0300

    refactor(telegram-bot): command vo types

--- apps/telegram-bot/application/src/dtos/commands/schedule-download.command.ts
+++ apps/telegram-bot/application/src/dtos/commands/schedule-download.command.ts
@@ -1,13 +1,16 @@
 import { Command } from "./base.command";
+import type { ResourceSourceUrl } from "@apps/telegram-bot-domain";
+import type { TelegramChatId } from "@apps/telegram-bot-domain";
+import type { TelegramMessageId } from "@apps/telegram-bot-domain";
 
 export class ScheduleDownloadCommand extends Command {
   constructor(
     // TODO valueobject
-    public readonly resourceUrl: string,
+    public readonly resourceUrl: ResourceSourceUrl,
     // TODO valueobject
-    public readonly chatId: string,
+    public readonly chatId: TelegramChatId,
     // TODO valueobject
-    public readonly messageId: string
+    public readonly messageId: TelegramMessageId
   ) {
     super()
   }
