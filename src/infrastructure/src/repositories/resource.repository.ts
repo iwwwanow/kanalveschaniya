0aa9d507d407e3446191c67ab01c17d3e00bbac8
Kirill Ivanov <iwwwanow.work@yandex.ru>
2025-12-07 17:11:10 +0300

    feat(infra): resource repo base structure

---
+++ src/infrastructure/src/repositories/resource.repository.ts
@@ -0,0 +1,24 @@
+import type { Resource } from '@domain';
+import type { ResourceId } from '@domain';
+import type { ResourceSourceUrl } from '@domain';
+import type { ResourceRepository } from '@domain';
+
+export class ResourceRepositoryImpl implements ResourceRepository {
+  async save(resource: Resource): Promise<void> {
+    // TODO
+  }
+
+  async findById(resourceId: ResourceId): Promise<Resource | null> {
+    // TODO
+  }
+
+  async findBySourceUrl(
+    resourceSourceUrl: ResourceSourceUrl,
+  ): Promise<Resource | null> {
+    // TODO
+  }
+
+  async delete(resourceId: ResourceId): Promise<void> {
+    // TODO
+  }
+}
