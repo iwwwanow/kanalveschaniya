88b5c44af813eb5b1ce48b7555c8dfeb7c4d5211
Kirill Ivanov <iwwwanow.work@yandex.ru>
2026-02-10 21:23:47 +0300

    feat(storage): drizzle db init

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import { ScheduleDownloadUseCase } from './app';
 import { TelegramEntrypoint } from './entrypoints';
 import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/libsql';
 
 const main = () => {
   console.log('server startup');
+
+  const db = drizzle(process.env.DB_FILE_NAME!);
 
   const scheduleDownloadUseCase = new ScheduleDownloadUseCase();
 
---
+++ src/storage/schema.ts
@@ -0,0 +1,30 @@
+// src/storage/db/schema.ts
+import { sql } from 'drizzle-orm';
+import {
+  sqliteTable,
+  integer,
+  text,
+  primaryKey,
+} from 'drizzle-orm/sqlite-core';
+
+export const resources = sqliteTable('resources', {
+  id: integer('id').primaryKey({ autoIncrement: true }),
+  url: text('url').notNull().unique(),
+  status: text('status', {
+    enum: ['pending', 'processing', 'downloaded', 'error'],
+  }).default('pending'),
+  dateUpdated: integer('date_updated', { mode: 'timestamp' }).default(
+    sql`(strftime('%s', 'now'))`,
+  ),
+});
+
+export const queue = sqliteTable('queue', {
+  id: integer('id').primaryKey({ autoIncrement: true }),
+  resourceId: integer('resource_id')
+    .notNull()
+    .references(() => resources.id, { onDelete: 'cascade' }),
+  scheduledAt: integer('scheduled_at', { mode: 'timestamp' }).default(
+    sql`(strftime('%s', 'now'))`,
+  ),
+  processedAt: integer('processed_at', { mode: 'timestamp' }),
+});
