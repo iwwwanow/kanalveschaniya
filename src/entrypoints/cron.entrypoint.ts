f986c68537b8844c4c8e6f1564c66c564f380fd2
Kirill Ivanov <iwwwanow.work@yandex.ru>
2026-02-11 21:49:56 +0300

    feat(entrypoints): cron

--- src/entrypoints/cron.entrypoint.ts
+++ src/entrypoints/cron.entrypoint.ts
@@ -1,1 +1,14 @@
-export class CronEntrypoint {}
+import type { ProcessDownloadUseCase } from '../app';
+import cron from 'node-cron';
+
+export class CronEntrypoint {
+  constructor(private processDownloadUseCase: ProcessDownloadUseCase) {}
+
+  start() {
+    const cronJob = cron.schedule('*/5 * * * *', async () => {
+      console.log('[Cron] Triggering queue processing...');
+      await this.processDownloadUseCase.execute(10);
+    });
+    cronJob.start();
+  }
+}
