2c4491584b286fefd8114709d768c6ecfb0854ef
Kirill Ivanov <iwwwanow.work@yandex.ru>
2026-08-23 16:37:06 +0300

    feat(common): telegraf proxy

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,9 +1,11 @@
 import { Telegraf } from "telegraf";
+import { SocksProxyAgent } from "socks-proxy-agent";
 import { config } from "../config";
 import { registerHandlers } from "./handlers";
 
 export function createBot() {
-  const bot = new Telegraf(config.botToken);
+  const agent = config.proxy ? new SocksProxyAgent(config.proxy) : undefined;
+  const bot = new Telegraf(config.botToken, agent ? { telegram: { agent } } : undefined);
   registerHandlers(bot, config.channelId);
   return bot;
 }
