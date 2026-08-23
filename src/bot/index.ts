58b43a9d5690300547ba3fadea69764227740014
Kirill Ivanov <iwwwanow.work@yandex.ru>
2026-08-23 16:55:21 +0300

    fix(proxy): agent polling

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -4,7 +4,7 @@ import { config } from "../config";
 import { registerHandlers } from "./handlers";
 
 export function createBot() {
-  const agent = config.proxy ? new SocksProxyAgent(config.proxy) : undefined;
+  const agent = config.proxy ? new SocksProxyAgent(config.proxy, { keepAlive: false }) : undefined;
   const bot = new Telegraf(config.botToken, agent ? { telegram: { agent } } : undefined);
   registerHandlers(bot, config.channelId);
   return bot;
